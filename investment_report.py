60db8224a16cc5907f9365741e131a66c33a35ad
ALAINA <alainaajce93@gmail.com>
2025-01-12 14:03:51 +0530

    updated file

--- investment_report.py
+++ investment_report.py
@@ -1,17 +1,31 @@
-# calculating loss or profit on investments
-def generate_report(investments):
-    print("Investment Report")
-    print("Name\t      Invested\tCurrent Value\tProfit/Loss |")
-    print("-" * 50)
-    for name, invested, current in investments:
-        profit_loss = current - invested
-        print(f"{name}\t{invested}\t   {current}\t\t  {profit_loss}")
+# calcilating profit or less of investment
 
-# dummy data
-investments = [
-    ("Stock A      ", 1000  ,  1200),
-    ("Stock B      ", 1500  ,  1300),
-    ("Mutual Fund C", 2000  ,  2500)
-]
+print("\n\tInvestment Report")
+print("\n3Name\tInvested\tCurrent Value\tProfit/Loss")
+print("-" * 40)
 
-generate_report(investments)
+total_invested = 0
+total_current = 0
+
+num = int(input("How many investments? "))
+for _ in range(num):
+    name = input("Investment name: ")
+    invested = float(input("Amount invested: "))
+    current = float(input("Current value: "))
+    profit_loss = current - invested
+    if profit_loss < 0:
+        print("Loss")
+    else:
+        print("Profit")
+
+    total_invested += invested
+    total_current += current
+    print(f"{name}\t{invested}\t{current}\t{profit_loss}")
+if profit_loss < 0:
+        print("Loss")
+else:
+        print("Profit")
+
+total_profit_loss = total_current - total_invested
+print("-" * 40)
+print(f"Total\t{total_invested}\t{total_current}\t{total_profit_loss}")
