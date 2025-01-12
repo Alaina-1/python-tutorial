9bce19e1cffc66c1d55e30e67b491a2a930fe4e2
ALAINA <alainaajce93@gmail.com>
2025-01-12 14:29:09 +0530

    final file

--- investment_report.py
+++ investment_report.py
@@ -1,31 +1,34 @@
-# calcilating profit or less of investment
+# calculating profit or loss of investment
 
-print("\n\tInvestment Report")
-print("\n3Name\tInvested\tCurrent Value\tProfit/Loss")
-print("-" * 40)
+print("\n\t\t\tInvestment Report")
+print("-" * 70)
 
 total_invested = 0
 total_current = 0
 
 num = int(input("How many investments? "))
 for _ in range(num):
-    name = input("Investment name: ")
+    name = input("\nInvestment name: ")
     invested = float(input("Amount invested: "))
     current = float(input("Current value: "))
     profit_loss = current - invested
-    if profit_loss < 0:
-        print("Loss")
-    else:
-        print("Profit")
-
     total_invested += invested
     total_current += current
-    print(f"{name}\t{invested}\t{current}\t{profit_loss}")
-if profit_loss < 0:
-        print("Loss")
-else:
-        print("Profit")
+    print("\nName\tInvested\tCurrent Value\tProfit/Loss")
+    print(f"{name}\t{invested}\t\t{current}\t\t{profit_loss}")
+    if profit_loss < 0:
+        print("\t\t\t\t\tLoss")
+    elif profit_loss > 0:
+         print("\t\t\t\t\tProfit")
+    else:
+        print("\t\t\t\t\tNo profit or loss")
 
 total_profit_loss = total_current - total_invested
-print("-" * 40)
-print(f"Total\t{total_invested}\t{total_current}\t{total_profit_loss}")
+print("-" * 70)
+print(f"Total\t{total_invested}\t\t{total_current}\t\t{total_profit_loss}")
+if total_profit_loss < 0:
+    print("\t\t\t\t\t  Loss")
+elif total_profit_loss > 0:
+    print("\t\t\t\t\t  Profit")
+else:
+    print("\t\t\t\t\tNo profit or loss")
