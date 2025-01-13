16a67077387f0d55524b9d861bf87daa24d0dd4f
ALAINA <alainaajce93@gmail.com>
2025-01-13 21:43:09 +0530

    income_tax

--- income_tax.py
+++ income_tax.py
@@ -1,12 +1,13 @@
 income = float(input("Enter your income: "))
-if income <= 250000:
+if income <= 25000:
     tax = 0
-elif income <= 500000:
-    tax = 0.05 * (income - 250000)
-elif income <= 1000000:
-    tax = 12500 + 0.1 * (income - 500000)
-elif income <= 1500000:
-    tax = 75000 + 0.15 * (income - 1000000)
+elif income <= 50000:
+    tax = 0.05 * (income - 25000)
+elif income <= 100000:
+    tax = 1250 + 0.1 * (income - 50000)  # 1250 comes from 0.05 * 25000
+elif income <= 150000:
+    tax = 6250 + 0.15 * (income - 100000)  # 6250 comes from 1250 + 0.1 * 50000
 else:
-    tax = 187500 + 0.2 * (income - 1500000)
+    tax = 11250 + 0.2 * (income - 150000)  # 11250 comes from 6250 + 0.15 * 50000
+
 print("Income tax: ", tax)
