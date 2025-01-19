045fd016a6628e2e77b5708b839c6285d3a69086
ALAINA <alainaajce93@gmail.com>
2025-01-20 00:15:32 +0530

    square_root

--- square_root.py
+++ square_root.py
@@ -1,6 +1,12 @@
-num = int(input("Enter a number: "))
+number = int(input("Enter a number: "))
 if num < 0:
-    print("Square root is not possible for negative numbers")
+    print("Square root is not possible for negative numbers ")
 else:
-    print("Square root is : ", num ** 0.5)
+    for  num in number/2:
+        z=1
+        if number==z*z:
+            break
+        z=z+1
+    
+print("root:",z)
     
