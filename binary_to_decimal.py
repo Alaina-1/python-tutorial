437c9a01b6779a015c0f6e16ca04747b5c5271be
ALAINA <alainaajce93@gmail.com>
2025-01-20 01:40:52 +0530

    binary_decimal

--- binary_to_decimal.py
+++ binary_to_decimal.py
@@ -1,7 +1,7 @@
 binary =input("give the binary digits:")
 position=0
 decimal=0
-for num in binary:
-    decimal += num*(2^position)
+for num in binary[::-1]:
+    decimal += int(num) *(2**position)
     position+=1
-print("decimal:", result )
+print("decimal: ", decimal )
