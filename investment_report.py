cbb84f4b6bb621bdc6158f48ba7c7ff3dc7388fd
ALAINA <alainaajce93@gmail.com>
2025-01-20 00:08:06 +0530

    investment_report

--- investment_report.py
+++ investment_report.py
@@ -1,7 +1,11 @@
-p=float(input("investment amount:"))
-i=float(input("intrest in precentile :"))
-t=int(input("year:"))
+investment=float(input("investment amount: "))
+interest=float(input("annual interest rate: "))
+year=int(input("years: "))
 
-si=float(p*i*t)/100
-
-print("total amnt:=",p+si)
+print("\nyear\t rate")
+print("-------------------")
+for y in range(1,year+1):
+    si=float(investment*interest)/100
+    investment=investment+si
+    print( y,"\t" ,round(investment,2))
+print("\n")
