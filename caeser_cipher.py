58a80e026d87e59630d41ea70a2939df2a50f97a
ALAINA <alainaajce93@gmail.com>
2025-01-20 01:18:36 +0530

    caesar_cipher

--- caeser_cipher.py
+++ caeser_cipher.py
@@ -11,3 +11,14 @@ for char in word:
     encrypted_message += newchar
 print("message:" ,encrypted_message)
 
+decrypted_message=""
+for char in encrypted_message:
+    ordinal_ch=ord(char)
+    ordinal_ch-=code
+    if( ordinal_ch<ord('a')):
+        ordinal_ch= ord('z')-(ord('a')-ordinal_ch-1)
+    newchar=chr(ordinal_ch)
+    decrypted_message+=newchar
+print("____DECRYPTION____")
+print("orginal message  ;",decrypted_message)
+    
