a364102dbf607f162111fb08eeb2c1839fb7eae6
ALAINA <alainaajce93@gmail.com>
2025-01-19 23:08:42 +0530

    investment_report

--- investment_report.py
+++ investment_report.py
@@ -1,34 +1,7 @@
-# calculating profit or loss of investment
+p=float(input("investment amount:"))
+i=float(input("intrest in precentile :"))
+t=int(input("year:"))
 
-print("\n\t\t\tInvestment Report")
-print("-" * 70)
+si=float(p*i*t)/100
 
-total_invested = 0
-total_current = 0
-
-num = int(input("How many investments are there? "))
-for _ in range(num):
-    name = input("\nInvestment name: ")
-    invested = float(input("Amount invested: "))
-    current = float(input("Current value: "))
-    profit_loss = current - invested
-    total_invested += invested
-    total_current += current
-    print("\nName\tInvested\tCurrent Value\tProfit/Loss")
-    print(f"{name}\t{invested}\t\t{current}\t\t{profit_loss}")
-    if profit_loss < 0:
-        print("\t\t\t\t\tLoss")
-    elif profit_loss > 0:
-         print("\t\t\t\t\tProfit")
-    else:
-        print("\t\t\t\t\tNo profit or loss")
-
-total_profit_loss = total_current - total_invested
-print("-" * 70)
-print(f"Total\t{total_invested}\t\t{total_current}\t\t{total_profit_loss}")
-if total_profit_loss < 0:
-    print("\t\t\t\t\t  Loss")
-elif total_profit_loss > 0:
-    print("\t\t\t\t\t  Profit")
-else:
-    print("\t\t\t\t\tNo profit or loss")
+print("total amnt:=",p+si)
