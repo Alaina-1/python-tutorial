4acd50149aaba371abfa53b9f72522340cb3d7ac
ALAINA <alainaajce93@gmail.com>
2025-01-27 13:02:04 +0530

    flesch_index

--- flesch/flesch_index.py
+++ flesch/flesch_index.py
@@ -1,4 +1,4 @@
-inp= open("input.txt", "r")
+inp = open("input.txt", "r")
 
 text = inp.read()
 
@@ -10,6 +10,8 @@ syllables, let = 0, 0
 
 for word in words:
     word = word.strip(",!?.\n").lower()
+    
+    
     for (index, letter) in enumerate(word):
         let += 1
         if let > 3:
@@ -19,16 +21,15 @@ for word in words:
         if letter in "aeiou":
             syllables += 1
             let = 0
-            if index > 0 and word[index-1] == letter:
+            if index > 0 and word[index - 1] == letter:
                 syllables -= 1
-        
-    if word[-2:] == "es" or word[-2:] == "ed" or (word [-1] == "e" and word[-2] != "l"):
-        syllables -= 1
-
+    if len(word) >= 2:
+        if word[-2:] == "es" or word[-2:] == "ed" or (word[-1] == "e" and word[-2] != "l"):
+            syllables -= 1
     if syllables <= 0:
         syllables = 1
 
-flesch = 835.206 - (1.015*(word_len/sentence_len)) - (84.6*(syllables / word_len))
+flesch = 835.206 - (1.015 * (word_len / sentence_len)) - (84.6 * (syllables / word_len))
 grade = 0.39 * (word_len / sentence_len) + 11.8 * (syllables / word_len) - 15.59
 
 print("Flesch score:", flesch)
