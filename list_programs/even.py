b01964f8616a690cae1edabb8b241557aeb312ae
ALAINA <alainaajce93@gmail.com>
2025-02-04 20:06:19 +0530

    list_programs

--- list_programs/even.py
+++ list_programs/even.py
@@ -1,4 +1,5 @@
-a=[3,1,4,5,2,8,5,9]
-even=[num for num in a if num%2==0]
+num_list = list(map(int, input("Enter numbers separated by space: ").split()))
+print(num_list)
+even=[num for num in num_list if num%2==0]
 even.sort()
 print("the even numbers are:  ",even)
