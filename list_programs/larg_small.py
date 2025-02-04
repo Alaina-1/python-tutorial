660a7ddc7c66fdbf19d8117c212bdc17c52f895b
ALAINA <alainaajce93@gmail.com>
2025-02-04 19:31:55 +0530

    list_programs

---
+++ list_programs/larg_small.py
@@ -0,0 +1,16 @@
+a=[1,5,3,4,2]
+min=a[0]
+for i in a:
+    if (i<min):
+        min=i
+print("smallest number ",min)
+max=a[0]
+for i in a:
+    if (i>max):
+        max=i
+print("largest number ",max)
+
+
+    
+
+
