2bfeb635c58176455b861dd2acea8f4ebe84cf61
ALAINA <alainaajce93@gmail.com>
2025-02-04 20:07:52 +0530

    list_programs

--- list_programs/filter.py
+++ list_programs/filter.py
@@ -1,6 +1,6 @@
-a=[1,2,3,4,5,6,7,8,9,10]
-print("list  :  ",a)
+num_list = list(map(int, input("Enter numbers separated by space: ").split()))
+print(num_list)
 
-check = int(input("enter the number to check"))
-new_list = [ num for num in a if num < check]
+check = int(input("enter the number to check "))
+new_list = [ num for num in num_list if num < check]
 print("Numbers less than ",check," are ",new_list)
