8fb3a5c0e781ca3c53abcc8eacc862fe4d85a377
ALAINA <alainaajce93@gmail.com>
2025-02-04 19:35:43 +0530

    list_programs

--- list_programs/larg_small.py
+++ list_programs/larg_small.py
@@ -1,15 +1,8 @@
 a=[1,5,3,4,2]
-min=a[0]
-for i in a:
-    if (i<min):
-        min=i
-print("smallest number ",min)
-max=a[0]
-for i in a:
-    if (i>max):
-        max=i
-print("largest number ",max)
 
+print("list",a)
+print("largest number is :",max(a))
+print("smallest number is :",min(a))
 
     
 
