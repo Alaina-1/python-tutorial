568b30287012625dc0cdaf14fdd9d994dc2e20c7
ALAINA <alainaajce93@gmail.com>
2025-02-04 20:04:31 +0530

    list_programs

--- list_programs/common.py
+++ list_programs/common.py
@@ -1,4 +1,6 @@
-a=[2,5,1,4,9,3,6,7]
-b=[5,3,7,0]
-common = [i for i in a if i in b]
-print(common)
+num_list1 = list(map(int, input("Enter numbers separated by space: ").split()))
+print(num_list1)
+num_list2 = list(map(int, input("Enter numbers separated by space: ").split()))
+print(num_list2)
+common = [i for i in num_list1 if i in num_list2]
+print("common elements are :  ",common)
