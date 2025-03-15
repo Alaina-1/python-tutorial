c7bbd94787446bb86be75b28b42af9d2f080428d
ALAINA <alainaajce93@gmail.com>
2025-03-15 21:45:37 +0530

    guess

--- guess/guess.py
+++ guess/guess.py
@@ -7,12 +7,8 @@ class GuessingGame(EasyFrame):
         super().__init__(title="Number Guessing Game")
         self.setSize(400, 200)
         self.setResizable(False)
-
-        # Generate a random number
         self.secret_number = random.randint(1, 100)
         self.attempts = 0
-
-        # UI Elements
         self.addLabel("Guess a number between 1 and 100!", row=0, column=0, columnspan=2, sticky="NSEW")
         self.inputField = self.addIntegerField(value=0, row=1, column=0, sticky="EW")
         self.addButton("Check", row=1, column=1, command=self.check_guess)
@@ -29,7 +25,7 @@ class GuessingGame(EasyFrame):
             self.resultLabel["text"] = "Try a lower number!"
         else:
             self.resultLabel["text"] = f"🎉 Correct! You guessed it in {self.attempts} tries."
-            self.inputField["state"] = "disabled"  # Disable input after winning
+            self.inputField["state"] = "disabled"  
 
 if __name__ == "__main__":
     GuessingGame().mainloop()
